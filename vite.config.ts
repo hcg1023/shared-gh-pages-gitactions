dbe4e71386339911f1a006c6eeb15cd6daa0a71a
haochenguang <2293885211@qq.com>
2023-08-29 16:25:01 +0800

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+
+let basePath = '/'
+
+if (isGithubActions) {
+  // 去掉 `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  basePath = `/${repo}`
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/shared-gh-pages-gitactions',
+  base: basePath,
   plugins: [vue()],
 })
